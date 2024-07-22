1f06743e724412a5faaaa4948a11aa71c547a641
Shalev <Shalev0125@gmail.com>
2024-07-22 15:48:32 +0300

    auth template project adaptation

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PostgresPrismaService } from './prisma/postgres/postgres.service';
+import { PostgresPrismaService } from './prisma/postgres.service';
 import { MongoPrismaService } from './prisma/mongo/mongo.service';
 
 @Module({
